c0ce7b435fef7cae6c724a7df3e19b390a2c3a5c
loveyu <loveyu22333@gmail.com>
2025-05-19 14:51:36 +0800

    删除delete_at

--- base_models.go
+++ base_models.go
@@ -2,18 +2,17 @@ package gb
 
 import (
 	"gorm.io/gorm"
-	"gorm.io/plugin/soft_delete"
 	"time"
 )
 
 // BaseModel 基础模型，包含共同字段
 type BaseModel struct {
-	ID              int64                 `gorm:"column:id;type:bigint(20);primary_key;AUTO_INCREMENT" json:"id"`
-	CreatedAt       time.Time             `gorm:"column:created_at;type:datetime;default:CURRENT_TIMESTAMP;NOT NULL" json:"-"`
-	UpdatedAt       time.Time             `gorm:"column:updated_at;type:datetime;default:CURRENT_TIMESTAMP;NOT NULL" json:"-"`
-	DeletedAt       soft_delete.DeletedAt `gorm:"column:deleted_at;type:datetime;default:0" json:"-"`
-	CreatedAtFormat string                `json:"created_at" gorm:"-"`
-	UpdatedAtFormat string                `json:"updated_at" gorm:"-"`
+	ID              int64     `gorm:"column:id;type:bigint(20);primary_key;AUTO_INCREMENT" json:"id"`
+	CreatedAt       time.Time `gorm:"column:created_at;type:datetime;default:CURRENT_TIMESTAMP;NOT NULL" json:"-"`
+	UpdatedAt       time.Time `gorm:"column:updated_at;type:datetime;default:CURRENT_TIMESTAMP;NOT NULL" json:"-"`
+	CreatedAtFormat string    `json:"created_at" gorm:"-"`
+	UpdatedAtFormat string    `json:"updated_at" gorm:"-"`
+	//DeletedAt       soft_delete.DeletedAt `gorm:"column:deleted_at;softDelete:milli;uniqueIndex:deleted_at_index" json:"-"`
 }
 
 // AfterFind 在查询后自动调用，格式化时间
