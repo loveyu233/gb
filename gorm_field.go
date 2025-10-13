340a80e925f7d56ba82cbdddeed8f1f42911d098
loveyu <loveyu22333@gmail.com>
2025-10-13 10:51:27 +0800

    📈 perf: 增加gen常用的方法

---
+++ gorm_field.go
@@ -0,0 +1,21 @@
+package gb
+
+import (
+	"gorm.io/datatypes"
+	"gorm.io/gen/field"
+)
+
+// GenJSONArrayQuery datatypes.JSONArrayQuery(columnName)
+func GenJSONArrayQuery(columnName string) *datatypes.JSONArrayExpression {
+	return datatypes.JSONArrayQuery(columnName)
+}
+
+// GenNewTime field.NewTime(tableName, columnName)
+func GenNewTime(tableName string, columnName string) field.Expr {
+	return field.NewTime(tableName, columnName)
+}
+
+// GenNewUnsafeFieldRaw field.NewUnsafeFieldRaw(rawSQL, vars...)
+func GenNewUnsafeFieldRaw(rawSQL string, vars ...interface{}) field.Field {
+	return field.NewUnsafeFieldRaw(rawSQL, vars...)
+}
