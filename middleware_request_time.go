f49f23d9caf882967fdfe4c40945918bd986f78b
loveyu <loveyu22333@gmail.com>
2025-10-21 10:31:04 +0800

    🛠️ fix: 添加请求耗时统计并设置响应头

--- middleware_request_time.go
+++ middleware_request_time.go
@@ -1,10 +1,17 @@
 package gb
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 // MiddlewareRequestTime 注入请求时间
 func MiddlewareRequestTime() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Set("request_time", Now())
+		startTime := Now()
+		c.Set("request_time", startTime)
+		c.Next()
+		c.Header("response_time", fmt.Sprintf("%dms", Now().Sub(startTime).Milliseconds()))
 	}
 }
