2153b47e9ec22e9071f4ae44200df1277766f44f
loveyu <loveyu22333@gmail.com>
2025-11-20 14:00:31 +0800

    🐛 fix(core): 调整手机号脱敏为3-4-4规则

--- mask.go
+++ mask.go
@@ -99,7 +99,7 @@ func MaskMobile(mobile string) string {
 	}
 
 	// 对手机号进行脱敏：前3位 + 5个* + 后3位
-	return mobile[:3] + "*****" + mobile[8:]
+	return mobile[:3] + "****" + mobile[7:]
 }
 
 // MaskIDCardCustom 自定义脱敏格式的身份证号处理
